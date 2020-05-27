8eb85c684b2a3213fe4516d6b9530110e5522a14
walshj9 <walshj9@students.rowan.edu>
2020-05-27 01:03:45 -0400

    Uses data from Input.html and uses Omdb's API and key to gather information about the entered movie

--- info_getter.php
+++ info_getter.php
@@ -1,20 +1,24 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = ""; 
+
 $title = $_GET['title']; 
 $year = $_GET['year']; 
 $url = 'http://www.omdbapi.com/?apikey=674c930e&t=';
-$test = '';
+$resp_link = '';
 $gross='';
 
 if (!is_null($year)){
 	$title =strtolower($title);
 	$title = ucwords($title);
 	$title= preg_replace('/\s+/', '+', $title);
-	$test = $url.$title;}
+	$resp_link = $url.$title;}
 else{
 	$title = preg_replace('/\s+/', '+', $title);
-	$test = $url.$title."&y=".$year;}
+	$resp_link = $url.$title."&y=".$year;}
 
-$response = file_get_contents($test);
+$response = file_get_contents($resp_link);
 $obj = json_decode($response);
 if(strcmp($obj->Response, "False")===0){
 	exit('Invalid Movie Title');
@@ -24,23 +28,20 @@ $release_year = $obj->Year;
 $movie_rating =$obj->Ratings[0]->Value;
 $poster_link = $obj->Poster;
 $gross = $obj->BoxOffice;
-
-$address = "localhost";
-$username = "root";
-$password = "";
-$server  = mysqqli_connect($address, $username, $password);
+//PRODUCTION
+$server  = mysqli_connect($servername, $username, $password, "movie_ratings", "3306");
 //, "movie_ratings", "3306"
-//print_r($server);//mysqlnd v7.4.2 
-$dupecheck = "SELECT * FROM movie_ratings WHERE (Title = '$movie_title' AND Release_Year = '$release_year')";
-$duperaw = mysql_query( $dupecheck); 
-// $server,
-if(mysql_num_rows($duperaw) >0 ) {
-      echo("$movie_title already exists in the table \n");} 
+//print_r($server);->mysqlnd v7.4.2 
+$dupecheck = "SELECT * FROM `movie_ratings` WHERE (Title = '$movie_title' AND Release_Year = '$release_year')";
+$duperaw = mysqli_query($server, $dupecheck); 
+ 
+if(!$duperaw || mysqli_num_rows($duperaw) > 0 ) { 
+       echo("$movie_title already exists in the table \n");
+}
 else{
-		$sql = "INSERT INTO Movie_Ratings (Title,Release_Year,IMDB_rating,BoxOffice,Poster_Link) VALUES('$movie_title','$release_year','$movie_rating','$gross', '$poster_link')";
-		if(mysql_query( $sql)){echo "Added {$movie_title} to DB";} 
-//$server,
-		else{echo "ERROR: Unable to execute sql. ";}}
-//	 . mysqli_error($server)
-mysql_close($server);
+		$sql = "INSERT INTO `movie_ratings` (Title,Release_Year,IMDB_rating,BoxOffice,Poster_Link) VALUES('$movie_title','$release_year','$movie_rating','$gross', '$poster_link')";
+		if(mysqli_query($server, $sql)){echo "Added {$movie_title} to DB";}
+		else{echo "ERROR: Unable to execute sql. ". mysqli_error($server);}}
+		
+mysqli_close($server);
 ?>
